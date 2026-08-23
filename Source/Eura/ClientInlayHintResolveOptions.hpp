dd95678d4e23d39a109a99993268620bb37db62a
verdeving <verodeving@gmail.com>
2026-08-24 00:25:17 +0200

    [Eura] Added ClientInlayHintResolveOptions

---
+++ Source/Eura/ClientInlayHintResolveOptions.hpp
@@ -0,0 +1,22 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if lyrix_major >= 0 and lyrix_middle >= 0 and lyrix_minor >= 1
+#ifndef lyrix_header_guard_eura_client_inlay_hint_resolve_options
+#define lyrix_header_guard_eura_client_inlay_hint_resolve_options
+#include "Eura/Json.hpp"
+
+namespace Eura
+{
+    struct [[nodiscard]] ClientInlayHintResolveOptions final
+    {
+        std::vector<std::string> properties;
+    };
+
+    auto from_json(const nlohmann::json& object, ClientInlayHintResolveOptions&
+    client_inlay_hint_resolve_options) noexcept -> void;
+
+    auto to_json(nlohmann::json& object, const ClientInlayHintResolveOptions&
+    client_inlay_hint_resolve_options) noexcept -> void;
+}
+
+#endif
+#endif
