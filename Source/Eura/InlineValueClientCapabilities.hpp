e4b0e2a32edd9b4bc1965b24c493b4d0a1820c50
verdeving <verodeving@gmail.com>
2026-08-24 00:18:02 +0200

    [Eura] Added InlineValueClientCapabilities

---
+++ Source/Eura/InlineValueClientCapabilities.hpp
@@ -0,0 +1,22 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if lyrix_major >= 0 and lyrix_middle >= 0 and lyrix_minor >= 1
+#ifndef lyrix_header_guard_eura_inline_value_client_capabilities
+#define lyrix_header_guard_eura_inline_value_client_capabilities
+#include "Eura/Json.hpp"
+
+namespace Eura
+{
+    struct [[nodiscard]] InlineValueClientCapabilities final
+    {
+        std::optional<bool> dynamicRegistration;
+    };
+
+    auto from_json(const nlohmann::json& object, InlineValueClientCapabilities&
+    inline_value_client_capabilities) noexcept -> void;
+
+    auto to_json(nlohmann::json& object, const InlineValueClientCapabilities&
+    inline_value_client_capabilities) noexcept -> void;
+}
+
+#endif
+#endif
