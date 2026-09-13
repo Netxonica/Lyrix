3f6179a88be27df4e2819c6aa2a9123ed9baaadb
verdeving <verodeving@gmail.com>
2026-09-13 11:48:34 +0200

    [Eura] Added SelectionRangeOptions

---
+++ Source/Eura/SelectionRangeRegistrationOptions.cpp
@@ -0,0 +1,22 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if lyrix_major >= 0 and lyrix_middle >= 0 and lyrix_minor >= 1
+#include "Eura/SelectionRangeRegistrationOptions.hpp"
+
+namespace Eura
+{
+    auto from_json(const nlohmann::json& object, SelectionRangeRegistrationOptions&
+    selection_range_registration_options) noexcept -> void
+    {
+        from_json(object, static_cast<SelectionRangeOptions&>(selection_range_registration_options)
+        );
+    }
+
+    auto to_json(nlohmann::json& object, const SelectionRangeRegistrationOptions&
+    selection_range_registration_options) noexcept -> void
+    {
+        to_json(object, static_cast<const SelectionRangeOptions&>(
+        selection_range_registration_options));
+    }
+}
+
+#endif
---
+++ Source/Eura/SelectionRangeRegistrationOptions.hpp
@@ -0,0 +1,21 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if lyrix_major >= 0 and lyrix_middle >= 0 and lyrix_minor >= 1
+#ifndef lyrix_header_guard_eura_selection_range_registration_options
+#define lyrix_header_guard_eura_selection_range_registration_options
+#include "Eura/SelectionRangeOptions.hpp"
+#include "Eura/StaticRegistrationOptions.hpp"
+#include "Eura/TextDocumentRegistrationOptions.hpp"
+
+namespace Eura
+{
+    struct [[nodiscard]] SelectionRangeRegistrationOptions final : SelectionRangeOptions{};
+
+    auto from_json(const nlohmann::json& object, SelectionRangeRegistrationOptions&
+    selection_range_registration_options) noexcept -> void;
+
+    auto to_json(nlohmann::json& object, const SelectionRangeRegistrationOptions&
+    selection_range_registration_options) noexcept -> void;
+}
+
+#endif
+#endif
