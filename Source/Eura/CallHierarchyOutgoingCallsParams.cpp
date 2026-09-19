694689a997c963b9bc66e903febe134239cab52e
verdeving <verodeving@gmail.com>
2026-09-20 00:35:23 +0200

    [Eura] Added CallHierarchyOutgoingCallsParams

---
+++ Source/Eura/CallHierarchyOutgoingCallsParams.cpp
@@ -0,0 +1,27 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if lyrix_major >= 0 and lyrix_middle >= 0 and lyrix_minor >= 1
+#include "Eura/CallHierarchyOutgoingCallsParams.hpp"
+
+namespace Eura
+{
+    auto from_json(const nlohmann::json& object, CallHierarchyOutgoingCallsParams&
+    call_hierarchy_outgoing_calls_params) noexcept -> void
+    {
+        from_json(object, static_cast<WorkDoneProgressParams&>(call_hierarchy_outgoing_calls_params
+        ));
+        from_json(object, static_cast<PartialResultParams&>(call_hierarchy_outgoing_calls_params));
+        call_hierarchy_outgoing_calls_params.item = object.at("item").get<CallHierarchyItem>();
+    }
+
+    auto to_json(nlohmann::json& object, const CallHierarchyOutgoingCallsParams&
+    call_hierarchy_outgoing_calls_params) noexcept -> void
+    {
+        to_json(object, static_cast<const WorkDoneProgressParams&>(
+        call_hierarchy_outgoing_calls_params));
+        to_json(object, static_cast<const PartialResultParams&>(
+        call_hierarchy_outgoing_calls_params));
+        object["item"] = call_hierarchy_outgoing_calls_params.item;
+    }
+}
+
+#endif
