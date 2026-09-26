071e6868afccecf3e59ba4b9e98886760abef4c7
verdeving <verodeving@gmail.com>
2026-09-26 17:14:31 +0200

    [Eura] Added InlineCompletionTriggerKind

---
+++ Source/Eura/InlineCompletionTriggerKind.hpp
@@ -0,0 +1,17 @@
+// Copyright 2026 Netxonica. All Rights Reserved.
+#if lyrix_major >= 0 and lyrix_middle >= 0 and lyrix_minor >= 1
+#ifndef lyrix_header_guard_eura_inline_completion_trigger_kind
+#define lyrix_header_guard_eura_inline_completion_trigger_kind
+#include <cstdint>
+
+namespace Eura
+{
+    enum class [[nodiscard]] InlineCompletionTriggerKind : std::uint8_t
+    {
+        Invoked = 1u,
+        Automatic = 2u
+    };
+}
+
+#endif
+#endif
